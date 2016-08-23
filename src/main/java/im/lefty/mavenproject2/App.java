0d6cb2ca54484486bcc6006476298f9f93610492
lefty <lefty@lefty-Inspiron-5558>
2016-08-23 05:25:09 +0100

    solj added

---
+++ src/main/java/im/lefty/mavenproject2/App.java
@@ -0,0 +1,13 @@
+package im.lefty.mavenproject2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
